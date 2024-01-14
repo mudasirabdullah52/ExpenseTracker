244006a1519b6171dafe3b4dac7934233360abd3
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2024-01-14 14:17:20 +0530

    complete project

--- dbConnection.js
+++ dbConnection.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('my-expense-tracker-db', 'root', 'Mudasir@1231', {
-    host: 'localhost',
-    dialect: 'mysql',
-
-});
-// const sequelize = new Sequelize('sql12676126', 'sql12676126', 'aNBSlu3dNY', {
-//     host: 'sql12.freemysqlhosting.net',
+// const sequelize = new Sequelize('my-expense-tracker-db', 'root', 'Mudasir@1231', {
+//     host: 'localhost',
 //     dialect: 'mysql',
 
 // });
+const sequelize = new Sequelize('sql12676126', 'sql12676126', 'aNBSlu3dNY', {
+    host: 'sql12.freemysqlhosting.net',
+    dialect: 'mysql',
+
+});
 module.exports = sequelize;
 
